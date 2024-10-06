9a52696dbec383cc0ce03a75559fca064e6d45a4
Naitik Agarwal <naitikagar12@gmail.com>
2024-10-06 11:53:20 +0530

    Add functionality to edit and add button

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     //TODO: fix modal-content not appearing as specified in taskInput.jsx
     <>
       <button onClick={toggleModal}>Add Task</button>
-      {isModalOpen ? <TaskInput isModalOpen={isModalOpen} toggleModal={toggleModal}></TaskInput> : null}
+      {isModalOpen ? <TaskInput isModalOpen={isModalOpen} toggleModal={toggleModal} taskTitle={""}></TaskInput> : null}
       <div>
         {tasks.map((task, index) => (
           <TaskElement 
@@ -27,7 +27,7 @@ function App() {
             taskDescription={task} 
             subTasks={task.subtasks}
             onDelete={() => console.log("Deleted")} 
-            onEdit={() => console.log("Edited")} 
+            // onEdit={() => console.log("Edited")} 
             addSubtask={() => console.log("Add Subtask")} 
             priority={'priority-high'}
           />
